73b48d49a7f1b2c4da95e673163783fed417d4cf
Randy0607 <randall.julies89@gmail.com>
2022-09-09 15:33:48 +0100

    update

--- monty.c
+++ monty.c
@@ -1,54 +1,61 @@
 
 #include "monty.h"
+#include <ctype.h>
 
-globales_t globalvar = {NULL, NULL, NULL};
+glbnfo *info = NULL;
 
 /**
- * main - entry point for the CLI program
- * @argc: count of arguments passed to the program
- * @argv: pointer to an array of char pointers to arguments
- * Return: EXIT_SUCCESS or EXIT_FAILURE
+ * main - our main function
+ * @argc: a count of the number of args
+ * @argv: an array of char pointers to arguments
+ * Return: 0 for success
  */
-
 int main(int argc, char **argv)
 {
-	char *token = NULL;
-	size_t line_buf_size = 0;
-	int line_number = 0, flag = 0, flag2 = 0;
-	ssize_t line_size;
 	stack_t *stack = NULL;
+	char *line_buf = NULL;
+	size_t line_buf_size = 0;
+	int line_size;
+	unsigned int line_count = 0;
+	int i = 0;
+	char *cmd_toks;
+	char *command[50];
+	char delimit[] = " \t\n";
+	const char *filename;
+	FILE *fp;
 
 	if (argc != 2)
-		stderr_usage();
-	globalvar.fd = fopen(argv[1], "r");
-	if (globalvar.fd == NULL)
-		stderr_fopen(argv[1]);
-	line_size = getline(&globalvar.line_buf, &line_buf_size, globalvar.fd);
-	if (globalvar.line_buf[0] == '#')
-		line_size = getline(&globalvar.line_buf, &line_buf_size, globalvar.fd);
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	filename = argv[1];
+	fp = fopen(filename, "r");
+	file_check(fp, filename);
+	info = malloc(sizeof(glbnfo));
+	if (info == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		fclose(fp);
+		exit(EXIT_FAILURE);
+	}
+	info->fp = &fp;
+	info->buffer = &line_buf;
+	line_size = getline(&line_buf, &line_buf_size, fp);
 	while (line_size >= 0)
-	{flag = 0;
-		flag2 = 0;
-		line_number++;
-		token = strtok(globalvar.line_buf, DELIM);
-		globalvar.token2 = strtok(NULL, DELIM);
-		if (token == NULL)
-		{flag2 = 1;
-			nop(&stack, line_number); }
-		if (flag2 == 0)
+	{
+		line_count += 1;
+		cmd_toks = strtok(line_buf, delimit);
+		while (cmd_toks != NULL)
 		{
-			if (token[0] == '#')
-			{
-				line_size = getline(&globalvar.line_buf,
-						    &line_buf_size, globalvar.fd);
-				flag = 1; }}
-		if (flag == 0)
-		{get_builtin(token, &stack, line_number);
-			line_size = getline(&globalvar.line_buf, &line_buf_size,
-					    globalvar.fd); }}
-	free_dlistint(stack);
-	free(globalvar.line_buf);
-	globalvar.line_buf = NULL;
-	fclose(globalvar.fd);
-	return (EXIT_SUCCESS);
+			command[i] = cmd_toks;
+			cmd_toks = strtok(NULL, delimit);
+			i++;
+		}
+		cmd_comp(command, &stack, line_count);
+	    line_size = getline(&line_buf, &line_buf_size, fp);
+	    i = 0;
+	}
+	clean_up(info->fp, &stack, info->buffer);
+	return (0);
 }
