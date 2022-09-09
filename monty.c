80caed9d48b29e200e1253420a6ec0091e12f13c
Randy0607 <randall.julies89@gmail.com>
2022-09-09 15:36:51 +0100

    update

--- monty.c
+++ monty.c
@@ -1,61 +1,102 @@
-
 #include "monty.h"
-#include <ctype.h>
-
-glbnfo *info = NULL;
 
 /**
- * main - our main function
- * @argc: a count of the number of args
- * @argv: an array of char pointers to arguments
- * Return: 0 for success
+ * open_up - open a monty and validate input
+ * @argc: args count
+ * @filename: path to monty
  */
-int main(int argc, char **argv)
+void open_up(int argc, char *filename)
 {
-	stack_t *stack = NULL;
-	char *line_buf = NULL;
-	size_t line_buf_size = 0;
-	int line_size;
-	unsigned int line_count = 0;
-	int i = 0;
-	char *cmd_toks;
-	char *command[50];
-	char delimit[] = " \t\n";
-	const char *filename;
-	FILE *fp;
-
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file\n");
+		dprintf(STDERR_FILENO, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	filename = argv[1];
-	fp = fopen(filename, "r");
-	file_check(fp, filename);
-	info = malloc(sizeof(glbnfo));
-	if (info == NULL)
+	monty.file = fopen(filename, "r");
+	if (!monty.file)
 	{
-		fprintf(stderr, "Error: malloc failed\n");
-		fclose(fp);
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", filename);
 		exit(EXIT_FAILURE);
 	}
-	info->fp = &fp;
-	info->buffer = &line_buf;
-	line_size = getline(&line_buf, &line_buf_size, fp);
-	while (line_size >= 0)
-	{
-		line_count += 1;
-		cmd_toks = strtok(line_buf, delimit);
-		while (cmd_toks != NULL)
-		{
-			command[i] = cmd_toks;
-			cmd_toks = strtok(NULL, delimit);
-			i++;
-		}
-		cmd_comp(command, &stack, line_count);
-	    line_size = getline(&line_buf, &line_buf_size, fp);
-	    i = 0;
-	}
-	clean_up(info->fp, &stack, info->buffer);
-	return (0);
 }
+
+/**
+ * read_line - reads and executes each line of input from monty file
+ */
+void read_line(void)
+{
+	size_t len = 0;
+	ssize_t read;
+	char *opcode, *data;
+
+	while ((read = getline(&monty.line, &len, monty.file) != -1))
+	{
+		opcode = strtok(monty.line, " ");
+		if (*opcode == '#' || *opcode == '\n')
+		{
+			monty.line_number++;
+			continue;
+		}
+		else if (strcmp(opcode, "push") == 0)
+		{
+			data = strtok(NULL, " \n");
+			if (monty.is_queue)
+			{
+				push_queue(data);
+			}
+			else
+				push(data);
+		}
+		else
+			op_choose(&monty.stack, opcode);
+		monty.line_number++;
+	}
+}
+
+/**
+ * op_choose - find & call the function that corresponds with the opcode
+ * @stack: **pointer to stack
+ * @opcode: opcode from this line of our monty file
+ */
+void op_choose(stack_t **stack, char *opcode)
+{
+	int i;
+	char *op;
+	instruction_t fncs[] = {
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{"sub", sub},
+		{"div", div_op},
+		{"mul", mul},
+		{"mod", mod},
+		{"pchar", pchar},
+		{"pstr", pstr},
+		{"rotl", rotl},
+		{"rotr", rotr},
+		{"stack", stack_op},
+		{"queue", queue_op},
+		{NULL, NULL}
+	};
+
+	op = strtok(opcode, "\n");
+	for (i = 0; fncs[i].opcode; i++)
+	{
+		if (strcmp(op, fncs[i].opcode) == 0)
+		{
+			fncs[i].f(stack, monty.line_number);
+			return;
+		}
+	}
+	if (strcmp(opcode, "push"))
+	{
+		dprintf(STDERR_FILENO, "L%u: ", monty.line_number);
+		dprintf(STDERR_FILENO, "unknown instruction %s\n", opcode);
+	}
+	else
+		dprintf(STDERR_FILENO, "L%u: usage: push integer\n", monty.line_number);
+	exit(EXIT_FAILURE);
+}s
